b3ffd40a06bc1a3b4e897668c490cce6e07f0895
imac1 <imac1@imac1-imac.local>
2018-12-19 11:34:45 +0300

    Initial Commit

---
+++ qrpay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  qrpay
+//
+//  Created by imac1 on 19.12.2018.
+//  Copyright © 2018 imac1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
